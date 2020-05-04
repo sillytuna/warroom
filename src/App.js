dda171fd93df88ad38344a2a62672424412026a8
sillytuna <alex@tunatech.com>
2020-05-04 18:04:19 +0100

    basic table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import _ from 'lodash';
+
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
+import { metaboss_testdata } from './data.js';
+
+function listResults(results) {
+		// Get keys into an array
+		let resultArray = [];
+
+		_.forIn(results['1'], function(value, key) {
+				resultArray[parseInt(key)] = value;
+		});
+
+		return resultArray.map((round, index) =>
+			<tr>
+					<td>{index}</td>
+					<td>{round.winner}</td>
+					<td>{round.flec_score}</td>
+					<td>{round.skirge_score}</td>
+					<td>{round.flec_strategy_vote}</td>
+					<td>{round.skirge_strategy_vote}</td>
+					<td>{round.flec_action}</td>
+					<td>{round.skirge_action}</td>
+			</tr>
+		);
+}
+
+function Episode(props) {
+	return (
+		<div>
+			<Table striped bordered variant="dark">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>winner</th>
+						<th>flec score</th>
+						<th>skirge score</th>
+						<th>flec strategy</th>
+						<th>skirge strategy</th>
+						<th>flec action</th>
+						<th>skirge action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{listResults(props.outcomes.infiltration)}
+				</tbody>
+			</Table>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Episode outcomes={metaboss_testdata.data.outcomes} />
+		</Container>
+	);
 }
 
 export default App;
