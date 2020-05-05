9149a77328801923c07497c43659c13b5709e829
sillytuna <alex@tunatech.com>
2020-05-05 16:04:55 +0100

    refactor and improved ux

--- src/App.js
+++ src/App.js
@@ -4,167 +4,199 @@ import _ from 'lodash';
 
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
-//import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 
 import { metaboss_testdata, GAMEDATA } from './exampleData.js';
 
-const PHASES = [ "infiltration", "analysis", "assault" ];
+const PHASES = ["infiltration", "analysis", "assault"];
 const MAX_MATCH_COUNT = 3;
 const MAX_ROUND_COUNT = 8;
 
 const STRATEGIES = ["Fire", "Snow", "Water"];
 const STRATEGY_CHECK = {
-		"-": 			{ "-": null, "Fire": false, "Snow": false, "Water": false },
-		"Fire":		{ "-": true, "Fire": null, "Snow": true, "Water": false },
-		"Snow":		{ "-": true, "Fire": false, "Snow": null, "Water": true },
-		"Water":	{ "-": true, "Fire": true, "Snow": false, "Water": null }
+	"-": { "-": null, "Fire": false, "Snow": false, "Water": false },
+	"Fire": { "-": true, "Fire": null, "Snow": true, "Water": false },
+	"Snow": { "-": true, "Fire": false, "Snow": null, "Water": true },
+	"Water": { "-": true, "Fire": true, "Snow": false, "Water": null }
 };
 
+var flecTeamNames = { rogue: "rogue", sage: "sage", armr: "armr" };
 
-function strategyResult(a, b) {
-		return STRATEGY_CHECK[a][b];
-}
-
-function listResults(results) {
-		let resultArray = [];
-
-		for (let i = 0; i < MAX_ROUND_COUNT; i++) {
-				let round_num = (i+1).toString();
-
-				if (results[round_num] === undefined) {
-						break;
-				}
-
-				resultArray[i] = results[round_num];
-		}
-
-		return resultArray.map((round, index) => {
-				const HIGHLIGHT = "font-weight-bold";
-				let markup = { winner: { flec: "", skirge: ""}, strategy_winner: { flec: "", skirge: ""} };
-
-				markup.winner[round.winner] = HIGHLIGHT;
-				markup.strategy_winner[round._strategy_winner] = HIGHLIGHT;
-
-				return (
-					<tr key={index}>
-							<td>{index + 1}</td>
-							<td>{round.winner}</td>
-							<td className={markup.winner.flec}>{round.flec_score}</td>
-							<td className={markup.winner.skirge}>{round.skirge_score}</td>
-							<td className={markup.strategy_winner.flec}>{round.flec_strategy_vote}</td>
-							<td className={markup.strategy_winner.skirge}>{round.skirge_strategy_vote}</td>
-							<td>{round.flec_action}</td>
-							<td>{round.skirge_action}</td>
-					</tr>
-				)
-		});
+function InitGameData() {
+	_.forIn(GAMEDATA.flec, function (value, key) {
+		flecTeamNames[value.slug] = value.name;
+	});
 }
 
 function ProcessResults(data) {
 	let outcomes = data.outcomes;
 
-	let nameStrategy = (s) => (s != null) ? STRATEGIES[s]: "-";
+	let nameStrategy = (s) => (s != null) ? STRATEGIES[s] : "-";
 
-		const TEAM_ACTIONS = {
-				skirge: {
-					'0': GAMEDATA.actions.skirge['0'].name,
-					'1': GAMEDATA.actions.skirge['1'].name,
-					'2': GAMEDATA.actions.skirge['2'].name,
-					'3': GAMEDATA.actions.skirge['3'].name,
-				},
-				rogue: {
-					'0': GAMEDATA.actions.rogue['0'].name,
-					'1': GAMEDATA.actions.rogue['1'].name,
-					'2': GAMEDATA.actions.rogue['2'].name,
-					'3': GAMEDATA.actions.rogue['3'].name,
-				},
-				sage: {
-					'0': GAMEDATA.actions.sage['0'].name,
-					'1': GAMEDATA.actions.sage['1'].name,
-					'2': GAMEDATA.actions.sage['2'].name,
-					'3': GAMEDATA.actions.sage['3'].name,
-				},
-				armr: {
-					'0': GAMEDATA.actions.armr['0'].name,
-					'1': GAMEDATA.actions.armr['1'].name,
-					'2': GAMEDATA.actions.armr['2'].name,
-					'3': GAMEDATA.actions.armr['3'].name,
-				}
-		};
+	const TEAM_ACTIONS = {
+		skirge: {
+			'0': GAMEDATA.actions.skirge['0'].name,
+			'1': GAMEDATA.actions.skirge['1'].name,
+			'2': GAMEDATA.actions.skirge['2'].name,
+			'3': GAMEDATA.actions.skirge['3'].name,
+		},
+		rogue: {
+			'0': GAMEDATA.actions.rogue['0'].name,
+			'1': GAMEDATA.actions.rogue['1'].name,
+			'2': GAMEDATA.actions.rogue['2'].name,
+			'3': GAMEDATA.actions.rogue['3'].name,
+		},
+		sage: {
+			'0': GAMEDATA.actions.sage['0'].name,
+			'1': GAMEDATA.actions.sage['1'].name,
+			'2': GAMEDATA.actions.sage['2'].name,
+			'3': GAMEDATA.actions.sage['3'].name,
+		},
+		armr: {
+			'0': GAMEDATA.actions.armr['0'].name,
+			'1': GAMEDATA.actions.armr['1'].name,
+			'2': GAMEDATA.actions.armr['2'].name,
+			'3': GAMEDATA.actions.armr['3'].name,
+		}
+	};
 
-	let nameAction = (team, s) => (s != null) ? TEAM_ACTIONS[team][s]: "-";
+	let nameAction = (team, s) => (s != null) ? TEAM_ACTIONS[team][s] : "-";
 
-	_.forIn(outcomes, function(phase, key) {
-			_.forIn(phase, function(match, key) {
-				_.forIn(match, function(round, key) {
-						// Make sense of the results
-						round.flec_strategy_vote = nameStrategy(round.flec_strategy_vote);
-						round.skirge_strategy_vote = nameStrategy(round.skirge_strategy_vote);
+	_.forIn(outcomes, function (phase, key) {
+		_.forIn(phase, function (match, key) {
+			_.forIn(match, function (round, key) {
+				// Make sense of the results
+				round.flec_strategy_vote = nameStrategy(round.flec_strategy_vote);
+				round.skirge_strategy_vote = nameStrategy(round.skirge_strategy_vote);
 
-						round.flec_action = nameAction(round.flec_faction, round.flec_action);
-						round.skirge_action = nameAction("skirge", round.skirge_action);
+				round.flec_action = nameAction(round.flec_faction, round.flec_action);
+				round.skirge_action = nameAction("skirge", round.skirge_action);
 
-						// Add extra metadata
-						let result = strategyResult(round.flec_strategy_vote, round.skirge_strategy_vote);
-						if (result === true)
-								round._strategy_winner = "flec";
-						else if (result === false)
-								round._strategy_winner = "skirge";
-						else
-								round._strategy_winner = null;
+				// Add extra metadata
+				let result = strategyResult(round.flec_strategy_vote, round.skirge_strategy_vote);
+				if (result === true)
+					round._strategy_winner = "flec";
+				else if (result === false)
+					round._strategy_winner = "skirge";
+				else
+					round._strategy_winner = null;
 
-				});
 			});
+		});
+	});
+}
+
+function strategyResult(a, b) {
+	return STRATEGY_CHECK[a][b];
+}
+
+function displayRounds(rounds) {
+	let resultArray = [];
+
+	for (let i = 0; i < MAX_ROUND_COUNT; i++) {
+		let roundNum = (i + 1).toString();
+
+		if (rounds[roundNum] === undefined) {
+			break;
+		}
+
+		resultArray[i] = rounds[roundNum];
+	}
+
+	return resultArray.map((round, index) => {
+		const HIGHLIGHT = "font-weight-bold";
+		let markup = { winner: { flec: "", skirge: "" }, strategy_winner: { flec: "", skirge: "" } };
+
+		markup.winner[round.winner] = HIGHLIGHT;
+		markup.strategy_winner[round._strategy_winner] = HIGHLIGHT;
+
+		return (
+			<tr key={index}>
+				<td>{index + 1}</td>
+				<td>{round.winner}</td>
+				<td className={markup.winner.flec}>{round.flec_score}</td>
+				<td className={markup.winner.skirge}>{round.skirge_score}</td>
+				<td className={markup.strategy_winner.flec}>{round.flec_strategy_vote}</td>
+				<td className={markup.strategy_winner.skirge}>{round.skirge_strategy_vote}</td>
+				<td>{round.flec_action}</td>
+				<td>{round.skirge_action}</td>
+			</tr>
+		)
 	});
 }
 
 function Match(props) {
 	return (
-		<div>
-			<Table striped bordered variant="dark">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>winner</th>
-						<th>flec score</th>
-						<th>skirge score</th>
-						<th>flec strategy</th>
-						<th>skirge strategy</th>
-						<th>flec action</th>
-						<th>skirge action</th>
-					</tr>
-				</thead>
-				<tbody>
-					{listResults(props.match)}
-				</tbody>
-			</Table>
-		</div>
+		<Table striped bordered variant="dark">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>winner</th>
+					<th>flec score</th>
+					<th>skirge score</th>
+					<th>flec strategy</th>
+					<th>skirge strategy</th>
+					<th>flec action</th>
+					<th>skirge action</th>
+				</tr>
+			</thead>
+			<tbody>
+				{displayRounds(props.rounds)}
+			</tbody>
+		</Table>
 	);
 }
 
 function Phase(props) {
+	const HIGHLIGHT = "font-weight-bold";
 	let content = [];
 
 	for (let i = 0; i < MAX_MATCH_COUNT; i++) {
-		let match_num = (i+1).toString();
+		let matchNum = (i + 1).toString();
 
-		if (props.phase[match_num] !== undefined) {
-				content[i] = (
-					<React.Fragment key={i}>
-						<h4>Match {match_num}</h4>
-						<Match match={props.phase[match_num]} />
-					</React.Fragment>
-				);
+		if (props.matches[matchNum] !== undefined) {
+			let result = props.results[matchNum];
+			let markup = { winner: { flec: "", skirge: "" } };
+			let buttonMarkup = "";
+
+			if ((result.flec_round_wins + result.skirge_round_wins) === MAX_ROUND_COUNT) {
+				buttonMarkup = (result.winner === "flec") ? "success" : "danger";
+				markup.winner[result.winner] = HIGHLIGHT;
+			}
+			else {
+				buttonMarkup = "warning";
+			}
+
+			content[i] = (
+				<React.Fragment key={i}>
+					<Card>
+						<Card.Header>
+							<Accordion.Toggle as={Button} variant={buttonMarkup} eventKey={i.toString()}>
+								<span className={markup.winner.flec}>{flecTeamNames[result.flec_team]} {result.flec_round_wins}</span>
+								&nbsp;-&nbsp;
+								<span className={markup.winner.skirge}> {result.skirge_round_wins} Skirge</span>
+							</Accordion.Toggle>
+						</Card.Header>
+						<Accordion.Collapse eventKey={i.toString()}>
+							<Card.Body>
+								<Match rounds={props.matches[matchNum]} />
+							</Card.Body>
+						</Accordion.Collapse>
+					</Card>
+				</React.Fragment>
+			);
 		}
 		else {
-				break;
+			break;
 		}
 	}
 
 	return (
-		<div>
+		<Accordion>
 			{content}
-		</div>
+		</Accordion>
 	);
 }
 
@@ -172,18 +204,18 @@ function Episode(props) {
 	let content = [];
 
 	for (let i = 0; i < PHASES.length; i++) {
-		let phase_name = PHASES[i];
+		let phaseName = PHASES[i];
 
-		if (props.episode[phase_name] !== undefined) {
+		if (props.data.outcomes[phaseName] !== undefined) {
 			content[i] = (
 				<React.Fragment key={PHASES[i]}>
-					<h3>Phase {phase_name}</h3>
-					<Phase phase={props.episode[phase_name]} />
+					<h3>{GAMEDATA.conflicts[i.toString()].name}</h3>
+					<Phase matches={props.data.outcomes[phaseName]} results={props.data.results.match[phaseName]} />
 				</React.Fragment>
 			);
 		}
 		else {
-				break;
+			break;
 		}
 	}
 
@@ -195,12 +227,13 @@ function Episode(props) {
 }
 
 function App() {
+	InitGameData();
 	ProcessResults(metaboss_testdata.data);
 
 	return (
 		<Container fluid>
 			<h1>MetaBoss War Room: {metaboss_testdata.data.params.episode}</h1>
-			<Episode episode={metaboss_testdata.data.outcomes} />
+			<Episode data={metaboss_testdata.data} />
 		</Container>
 	);
 }
